3c1e2e999cf4e5395be94deeb9f080122b7d6070
spire-signpost <spire.signpost@gmail.com>
2017-07-30 12:42:54 +0100

    add v0.2 to node-todos-api app

---
+++ node/apps/node-todos-api/v0.2/server/dbms/mongoose-config.js
@@ -0,0 +1,19 @@
+/*
+  Node.js, MongoDB, and Mongoose Todo app - v0.2
+  - mongoose-config.js
+  - config and connect
+*/
+
+// require Mongoose
+var mongoose = require('mongoose');
+
+// specify preferred Promise library
+mongoose.Promise = global.Promise;
+//connect to MongoDB using Mongoose
+mongoose.connect('mongodb://localhost:27017/NodeTodoApp');
+
+
+// module exports
+module.exports = {
+  mongoose // ES6 shortcut for mongoose: mongoose
+};
